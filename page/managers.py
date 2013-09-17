da91c6c22924d157f283ebd51721798bd73bb81a
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2013-09-17 20:11:51 +0200

    page manager changes

--- page/managers.py
+++ page/managers.py
@@ -5,3 +5,12 @@ class PageManager(models.Manager):
 
     def get_published(self, slug):
         return self.get_query_set().filter(slug=slug).filter(is_published=True)
+
+    def get_published_alphabetically(self, slug):
+        return self.get_published(slug).order_by('-title')
+
+    def all_alphabetically(self):
+        return self.get_query_set().order_by('title')
+
+    def all_published_alphabetically(self):
+        return self.get_query_set().filter(is_published=True).order_by('title')
